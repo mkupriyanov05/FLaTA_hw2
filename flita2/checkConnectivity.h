6a3bcfc2afb670f292ac2a0c54c7d58bf76f2926
mkupriyanov05 <114899758+mkupriyanov05@users.noreply.github.com>
2023-06-01 21:42:25 +0300

    Add checkConnectivity.h

---
+++ flita2/checkConnectivity.h
@@ -0,0 +1,8 @@
+#ifndef FLITA2_CHECKCONNECTIVITY_H
+#define FLITA2_CHECKCONNECTIVITY_H
+
+#include "graphFunctions.h"
+
+void checkGraphConnection(Graph_t *graph);
+
+#endif //FLITA2_CHECKCONNECTIVITY_H
